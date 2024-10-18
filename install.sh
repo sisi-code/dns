9344f056cd3e4d4738b68992e8318bc9d84ac953
sisi-code <sdraldynshadmnsh3@gmail.com>
2024-10-19 00:06:48 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 apt update -y
-curl -o $PREFIX/bin/kl https://raw.githubusercontent.com/sisi-code/main/Sel.sh
+curl -o $PREFIX/bin/kl https://raw.githubusercontent.com/sisi-code/dns/main/Sel.sh
 chmod +x $PREFIX/bin/si
